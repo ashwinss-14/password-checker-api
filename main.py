a70ba8db2f9f3d3bb928693416f014c39ac96440
Ash <302530656+ashwinss-14@users.noreply.github.com>
2026-08-18 20:38:27 +0530

    first fastapi project

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return{"message":"hello ash","status":"learning fastapi"}
+
+class PasswordCheck(BaseModel):
+    password:str
+
+@app.post("/check-password")
+def check_password(data:PasswordCheck):
+    length=len(data.password)
+    return{"password_length":length}
